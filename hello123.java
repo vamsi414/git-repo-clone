d854e1094bef63779b262b25510dfe5943267245
vamsi414 <40313414+vamsi414@users.noreply.github.com>
2019-08-22 12:35:43 +0530

    Update hello123.java

--- hello123.java
+++ hello123.java
@@ -8,7 +8,7 @@ class hello123
   /* Using for loop to repeat instruction execution */
  
     for (c = 1; c <= 10; c++) {
-      System.out.println(c);
+      System.out.println(c); // print
     }
   }
 }
